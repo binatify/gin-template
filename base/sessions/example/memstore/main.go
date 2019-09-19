e4188e54a0e157c8de1aa9424560c873a37485c3
csxuejin <602296435@qq.com>
2019-09-19 22:28:45 +0800

    add session

---
+++ base/sessions/example/memstore/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"binatify.com/common/sessions"
+	"binatify.com/common/sessions/memstore"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	store := memstore.NewStore([]byte("secret"))
+	r.Use(sessions.Sessions("mysession", store))
+
+	r.GET("/incr", func(c *gin.Context) {
+		session := sessions.Default(c)
+		var count int
+		v := session.Get("count")
+		if v == nil {
+			count = 0
+		} else {
+			count = v.(int)
+			count++
+		}
+		session.Set("count", count)
+		session.Save()
+		c.JSON(200, gin.H{"count": count})
+	})
+	r.Run(":8000")
+}
